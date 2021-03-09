ca3a7196eefa96eda2376222d6c0e0a930a488de
jayjoson <80364581+jayjoson@users.noreply.github.com>
2021-03-09 15:13:38 -0800

    change netlify identity url

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-identity`,
       options: {
-        url: `https://artofjayjoson.netlify.app/`, // required!
+        url: `https://artofjayjoson.com/`, // required!
       },
     },
     {
